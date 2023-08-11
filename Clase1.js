67a55b2b46e029b9c5eaf6d63cdce187717f8e7f
Benjah94 <136342025+Benjah94@users.noreply.github.com>
2023-08-11 08:22:09 -0300

    Add files via upload

---
+++ Clase1.js
@@ -0,0 +1,27 @@
+alert ("Hola mundo");
+console.log('La suma es igual a', 7+5)
+console.log('La multiplicacion es igual a', 5*4)
+console.log('La resta es igual a',10-5)
+console.log('La division es igual a',6/2)
+let suma = 7 + 7
+alert ('Mi edad es ' +suma )
+let nombreAlumno
+nombreAlumno=prompt('Ingresa tu nombre')
+alert('Binvenido ' + nombreAlumno)
+let nombreDiez = '10'
+let nombreTres = '3'
+let resultadoMultiplicacion = nombreDiez * nombreTres
+console.log('La multiplicacion es igual a', nombreDiez*nombreTres)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
